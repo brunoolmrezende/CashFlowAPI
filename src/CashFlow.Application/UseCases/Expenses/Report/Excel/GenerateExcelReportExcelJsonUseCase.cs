7777908ff992c5ed912b052ee6cf2dd63089bbc9
brunoolmrezende <bruoolmrezende303@gmail.com>
2024-07-15 21:50:03 -0300

    creating excel file and setting the fonts

---
+++ src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
@@ -0,0 +1,19 @@
+﻿
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Report.Excel
+{
+    public class GenerateExcelReportExcelJsonUseCase : IGenerateExcelReportExcelJsonUseCase
+    {
+        public async Task<byte[]> Execute(DateOnly month)
+        {
+            var workbook = new XLWorkbook();
+
+            workbook.Author = "Bruno Rezende";
+            workbook.Style.Font.FontSize = 12;
+            workbook.Style.Font.FontName = "Times Nwe Roman";
+
+           var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+        }
+    }
+}
