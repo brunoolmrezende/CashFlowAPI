0ce85307015520e1f6440327a5aabc3d18abbdb0
brunoolmrezende <bruoolmrezende303@gmail.com>
2024-07-16 20:43:25 -0300

    adding header for the excel file

--- src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
@@ -1,4 +1,5 @@
 ﻿
+using CashFlow.Domain.Reports;
 using ClosedXML.Excel;
 
 namespace CashFlow.Application.UseCases.Expenses.Report.Excel
@@ -14,6 +15,27 @@ namespace CashFlow.Application.UseCases.Expenses.Report.Excel
             workbook.Style.Font.FontName = "Times Nwe Roman";
 
            var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+
+            InsertHeader(worksheet);
+        }
+
+        private void InsertHeader(IXLWorksheet worksheet)
+        {
+            worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
+            worksheet.Cell("B1").Value = ResourceReportGenerationMessages.DATE;
+            worksheet.Cell("C1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+            worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
+            worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION;
+
+            worksheet.Cells("A1:E1").Style.Font.Bold = true;
+
+            worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5C2B6");
+
+            worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
         }
     }
 }
