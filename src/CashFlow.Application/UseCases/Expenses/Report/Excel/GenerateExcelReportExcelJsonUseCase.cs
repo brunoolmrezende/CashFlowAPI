68ed4db1787e850b679c65fd901d80f16f8a4dab
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-07-16 22:03:44 -0300

    returning excel file

--- src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
@@ -17,8 +17,13 @@ namespace CashFlow.Application.UseCases.Expenses.Report.Excel
            var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
 
             InsertHeader(worksheet);
-        }
 
+            var file = new MemoryStream();
+            workbook.SaveAs(file);
+
+            return file.ToArray();
+        }
+        
         private void InsertHeader(IXLWorksheet worksheet)
         {
             worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
