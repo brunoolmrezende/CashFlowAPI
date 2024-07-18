02b52d8b9799cc24a21b00d328816f776772baf3
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-07-17 22:27:36 -0300

    adding expenses info into the excel file

--- src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/Excel/GenerateExcelReportExcelJsonUseCase.cs
@@ -1,7 +1,8 @@
-﻿
+﻿using CashFlow.Domain.Enums;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using ClosedXML.Excel;
+using PaymentType = CashFlow.Domain.Enums.PaymentType;
 
 namespace CashFlow.Application.UseCases.Expenses.Report.Excel
 {
@@ -30,12 +31,35 @@ namespace CashFlow.Application.UseCases.Expenses.Report.Excel
 
             InsertHeader(worksheet);
 
+            var row = 2;
+            foreach (var expense in expenses)
+            {
+                worksheet.Cell($"A{row}").Value = expense.Title;
+                worksheet.Cell($"B{row}").Value = expense.Date;
+                worksheet.Cell($"C{row}").Value = ConvertPaymentType(expense.PaymentType);
+                worksheet.Cell($"D{row}").Value = expense.Amount;
+                worksheet.Cell($"E{row}").Value = expense.Description;
+
+                row++;
+            }
+
             var file = new MemoryStream();
             workbook.SaveAs(file);
 
             return file.ToArray();
         }
         
+        private string ConvertPaymentType(PaymentType payment)
+        {
+            return payment switch
+            {
+                PaymentType.Cash => ResourceConvertPaymentType.CASH,
+                PaymentType.CreditCard => ResourceConvertPaymentType.CREDIT_CARD,
+                PaymentType.DebitCard => ResourceConvertPaymentType.DEBIT_CARD,
+                PaymentType.Pix => ResourceConvertPaymentType.PIX,
+                _ => string.Empty
+            };
+        }
         private void InsertHeader(IXLWorksheet worksheet)
         {
             worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
