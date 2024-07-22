63ff78f375e044b187e9e1494aa718d71e657ac7
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-07-21 21:53:58 -0300

    adding table for expenses and colors

--- src/CashFlow.Application/UseCases/Expenses/Report/Pdf/GenerateReportPdfJsonUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/Pdf/GenerateReportPdfJsonUseCase.cs
@@ -3,6 +3,7 @@ using CashFlow.Application.UseCases.Expenses.Report.Pdf.Fonts;
 using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
 using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
 using MigraDoc.Rendering;
 using PdfSharp.Fonts;
 using System.Reflection;
@@ -35,6 +36,13 @@ namespace CashFlow.Application.UseCases.Expenses.Report.Pdf
 
             var totalExpenses = expenses.Sum(expense => expense.Amount);
             CreateTotalSpentSection(page, month, totalExpenses);
+
+            foreach (var expense in expenses)
+            {
+                var table = CreateExpenseTable(page);
+
+
+            }
 
             return RenderDocument(document);
         }
@@ -97,6 +105,18 @@ namespace CashFlow.Application.UseCases.Expenses.Report.Pdf
             paragraph.AddLineBreak();
 
             paragraph.AddFormattedText($"{CURRENCY_SYMBOL} {totalExpenses}", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50 });
+        }
+
+        private Table CreateExpenseTable(Section page)
+        {
+            var table = page.AddTable();
+
+            table.AddColumn("195").Format.Alignment = ParagraphAlignment.Left;
+            table.AddColumn("80").Format.Alignment = ParagraphAlignment.Center;
+            table.AddColumn("120").Format.Alignment = ParagraphAlignment.Center;
+            table.AddColumn("120").Format.Alignment = ParagraphAlignment.Right;
+
+            return table;
         }
 
         private byte[] RenderDocument(Document document)
