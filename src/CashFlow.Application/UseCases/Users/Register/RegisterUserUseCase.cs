1b4326c28fc364851ce422f15212027d3fafd416
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-08-03 11:29:50 -0300

    feat: encrypting passwords

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Exception.ExceptionsBase;
 
 namespace CashFlow.Application.UseCases.Users.Register
@@ -8,15 +9,18 @@ namespace CashFlow.Application.UseCases.Users.Register
     public class RegisterUserUseCase : IRegisterUserUseCase
     {
         private readonly IMapper _mapper;
-        public RegisterUserUseCase(IMapper mapper)
+        private readonly IPasswordEncripter _passwordEncripter;
+        public RegisterUserUseCase(IMapper mapper, IPasswordEncripter passwordEncripter)
         {
             _mapper = mapper;
+            _passwordEncripter = passwordEncripter;
         }
         public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
         {
             Validate(request);
 
             var user = _mapper.Map<Domain.Entities.User>(request);
+            user.Password = _passwordEncripter.Encrypt(request.Password);
 
             return new ResponseRegisteredUserJson
             {
