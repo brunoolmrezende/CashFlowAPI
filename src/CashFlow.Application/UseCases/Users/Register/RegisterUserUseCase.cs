bbecff873ca665cbf95652db8d7e91a2264837fd
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-08-05 12:22:11 -0300

    feat: conclunding token test

--- src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
+++ src/CashFlow.Application/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -4,6 +4,7 @@ using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.User;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Exception;
 using CashFlow.Exception.ExceptionsBase;
 
@@ -16,18 +17,22 @@ namespace CashFlow.Application.UseCases.Users.Register
         private readonly IUserReadOnlyRepository _userReadOnlyRepository;
         private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IAccessTokenGenerator _tokenGenerator;
+
         public RegisterUserUseCase(
             IMapper mapper, 
             IPasswordEncripter passwordEncripter,
             IUserReadOnlyRepository userReadOnlyRepository,
             IUserWriteOnlyRepository userWriteOnlyRepository,
-            IUnitOfWork unitOfWork)
+            IUnitOfWork unitOfWork,
+            IAccessTokenGenerator tokenGenerator)
         {
             _mapper = mapper;
             _passwordEncripter = passwordEncripter;
             _userReadOnlyRepository = userReadOnlyRepository;
             _userWriteOnlyRepository = userWriteOnlyRepository;
             _unitOfWork = unitOfWork;
+            _tokenGenerator = tokenGenerator;
         }
         public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
         {
@@ -44,6 +49,7 @@ namespace CashFlow.Application.UseCases.Users.Register
             return new ResponseRegisteredUserJson
             {
                 Name = user.Name,
+                Token = _tokenGenerator.Generate(user)
             };
         }
 
--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -2,8 +2,10 @@
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Domain.Repositories.User;
 using CashFlow.Domain.Security.Cryptography;
+using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using CashFlow.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,8 +18,17 @@ namespace CashFlow.Infrastructure
         {
            AddDbContext(services, configuration);
            AddRepositories(services);
+           AddToken(services, configuration);
 
            services.AddScoped<IPasswordEncripter, Security.Cryptography.BCrypt>();
+        }
+
+        private static void AddToken(this IServiceCollection services, IConfiguration configuration)
+        {
+            var expirationTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpireMinutes");
+            var signinKey = configuration.GetValue<string>("Settings:Jwt:SigninKey");
+
+            services.AddScoped<IAccessTokenGenerator>(config => new JwtTokenGenerator(expirationTimeMinutes, signinKey!));
         }
 
         private static void AddRepositories(IServiceCollection services)
