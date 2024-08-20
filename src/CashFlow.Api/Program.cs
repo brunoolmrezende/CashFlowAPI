a5c22664ad0085fe523591bcc32121c46a641f8e
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-08-20 18:03:31 -0300

    Reading token from request

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 using CashFlow.Infrastructure.Extensions;
+using CashFlow.Domain.Security.Tokens;
+using CashFlow.Api.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -52,6 +54,10 @@ builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)))
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
+
+builder.Services.AddScoped<ITokenProvider, HttpContextTokenValue>();
+
+builder.Services.AddHttpContextAccessor();
 
 var signinKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigninKey");
 
