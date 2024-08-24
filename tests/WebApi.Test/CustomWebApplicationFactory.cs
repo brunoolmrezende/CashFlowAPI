ec8ea646bfa6a8d86800f97b3873bd06d151cd21
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-08-24 14:27:06 -0300

    Tests to recover all expenses

--- tests/WebApi.Test/CustomWebApplicationFactory.cs
+++ tests/WebApi.Test/CustomWebApplicationFactory.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Domain.Security.Cryptography;
+﻿using CashFlow.Domain.Entities;
+using CashFlow.Domain.Security.Cryptography;
 using CashFlow.Domain.Security.Tokens;
 using CashFlow.Infrastructure.DataAccess;
 using CommonTestUtilities.Entities;
@@ -45,14 +46,27 @@ namespace WebApi.Test
         public string GetToken() => _token;
         private void StartDatabase(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
         {
+            AddUsers(dbContext, passwordEncripter);
+            AddExpenses(dbContext, _user);
+
+            dbContext.SaveChanges();
+        }
+
+        private void AddUsers(CashFlowDbContext dbContext, IPasswordEncripter passwordEncripter)
+        {
             _user = UserBuilder.Build();
             _password = _user.Password;
 
             _user.Password = passwordEncripter.Encrypt(_user.Password);
 
             dbContext.Users.Add(_user);
+        }
 
-            dbContext.SaveChanges();
+        private void AddExpenses(CashFlowDbContext dbContext, User user)
+        {
+            var expense = ExpenseBuilder.Build(user);
+
+            dbContext.Expenses.Add(expense);
         }
     }
 }
